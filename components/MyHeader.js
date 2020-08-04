66d8d0d71ee7eca5924d699a2dee898fcba2e889
swastikk06 <royswastikk06@gmail.com>
2020-08-04 10:49:06 +0530

    added hamburger menue

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -1,14 +1,54 @@
 import React, { Component} from 'react';
 import { Header,Icon,Badge } from 'react-native-elements';
 import { View, Text, StyeSheet ,Alert} from 'react-native';
+import db from '../config'
+import config from '../config';
+class MyHeader extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      value:''
+    }
+  }
+  getNumberOfUnreadNotification(){
+    db.collection ('all_notifications').where('notification_status','==',"unread")
+    .onSnapshot((Snapshot)=>{
+      var unreadNotification=Snapshot.docs.map((doc)=>{
+doc.data()
+      })
+      this.setState({value:unreadNotification.length})
+      
+    })
+  }
+  componentDidMount(){
+    this.getNumberOfUnreadNotification()
+  }
+  bellIconWithBadge=()=>{
+return(
+  <View>
+    <Icon name='bell' type='font-awesome' color='#696969' size={25} onPress={()=>{
+      this.props.navigation.navigate('Notification')
+    }}>
 
-const MyHeader = props => {
-  return (
-    <Header
-      centerComponent={{ text: props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
-      backgroundColor = "#eaf8fe"
-    />
-  );
-};
+    </Icon>
+    <Badge value={this.state.value} containerStyle={{position:'absolute',top:-4,right:-4}}>
+
+    </Badge>
+  </View>
+)
+  }
+render(){
+  return(
+    <Header leftComponent={<Icon name='bars' type='font-awesome' color='#696969' onPress={()=>{
+      this.props.navigation.toggleDrawer()
+    }}></Icon>} centerComponent={{text:this.props.title,style:{color:'#90a5a9',fontSize:20,fontWeight:'bold'}}} rightComponent={<this.bellIconWithBadge{
+      ...this.props
+    }/>} backgroundColor='#eaf8fe'>  
+
+    </Header>
+  )
+}
+  
+}
 
 export default MyHeader;
